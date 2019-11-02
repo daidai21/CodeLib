f9292f019c88d8e25ebf31674bb004ec21080303
daidai21 <daidai4269@aliyun.com>
2019-11-02 18:20:53 +0800

    add sort_test && all sort algorithms test ok

--- reuse/Python3.x/Algorithm/sort/quick_sort.py
+++ reuse/Python3.x/Algorithm/sort/quick_sort.py
@@ -10,26 +10,29 @@
 # =============================================================================
 
 
-def quick_sort(lst, first, last):
-    if first >= last:
-        return 
-    mid_val = lst[first]
-    low = first
-    high = last
-    while low < high:
-        while low < high and lst[high] >= mid_val:
-            high -= 1
-        lst[low] = lst[high]
-        while low < high and lst[low] < mid_val:
-            low += 1
-        lst[high] = lst[low]
-    lst[low] = mid_val
-    quick_sort(lst, first, low - 1)
-    quick_sort(lst, low + 1, last)
+def quick_sort(lst):
+    def sort(lst, first, last):
+        if first >= last:
+            return 
+        mid_val = lst[first]
+        low = first
+        high = last
+        while low < high:
+            while low < high and lst[high] >= mid_val:
+                high -= 1
+            lst[low] = lst[high]
+            while low < high and lst[low] < mid_val:
+                low += 1
+            lst[high] = lst[low]
+        lst[low] = mid_val
+        sort(lst, first, low - 1)
+        sort(lst, low + 1, last)
+        return lst
 
+    return sort(lst, 0, len(lst) - 1)
 
 if __name__ == "__main__":
-    l = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-    quick_sort(l, 0, len(l) - 1)
+    lst = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    l = quick_sort(lst)
     print(l)
 
---
+++ reuse/Python3.x/Algorithm/sort/sort_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+# =============================================================================
+# File Name: sort_test.py
+# Author: DaiDai
+# Mail: daidai4269@aliyun.com
+# Created Time: Sat 02 Nov 2019 06:00:26 PM CST
+# =============================================================================
+
+
+from bubble_sort import bubble_sort
+from bucket_sort import bucket_sort
+from count_sort import count_sort
+from insert_sort import insert_sort
+from merge_sort import merge_sort
+from quick_sort import quick_sort
+
+import random
+import unittest
+
+
+LIST_LENGTH = 1000
+LIST_VALUE_MIN = -100
+LIST_VALUE_MAX = 100
+
+
+class TestMain(unittest.TestCase):
+    def _sort_template(self, func):
+        lst = [random.randint(LIST_VALUE_MIN, LIST_VALUE_MAX) for _ in range(LIST_LENGTH)]
+        return sorted(lst) == func(lst)
+
+    def test_all_sort(self):
+        for func in [bubble_sort, bucket_sort, count_sort, insert_sort, 
+        merge_sort, quick_sort]:
+            if self._sort_template(func):
+                print("OK ", str(func))
+            else:
+                assert False, "{} error".format(func)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
