e033dbd672416544dc52a7b06d110bf6c93cad6c
daidai21 <daidai4269@aliyun.com>
2019-12-12 17:17:10 +0800

    TODO all file, this is todo list

---
+++ reuse/C++/ThreadSynchronization/README.md
@@ -0,0 +1,11 @@
+### C++ STL with All version
+
+
+TODO: future
+
+
+TODO: (C++11/C++14 exclude)
+    semaphore
+    latch
+    barrier
+
---
+++ reuse/C++/ThreadSynchronization/thread_sync_test.cpp
@@ -0,0 +1,15 @@
+/* =============================================================================
+> File Name: thread_sync_test.cpp
+> Author: DaiDai
+> Mail: daidai4269@aliyun.com
+> Created Time: Thu 12 Dec 2019 05:10:54 PM CST
+============================================================================= */
+
+
+// test thread model performance
+
+#include <iostream>
+
+using namespace std;
+
+
