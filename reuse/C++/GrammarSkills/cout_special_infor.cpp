d8b41a336015eb094f8d793ca783d6adfb0bdbdc
daidai21 <daidai4269@aliyun.com>
2019-12-27 21:09:48 +0800

    R  reuse/C++/GrammarSkills/cout_special_number.cpp -> reuse/C++/GrammarSkills/cout_special_infor.cpp D  reuse/C++/GrammarSkills/cout_special_string.cpp D  reuse/C++/GrammarSkills/print_special_symbols.cpp A  template/C++.project/include/c.h

--- reuse/C++/GrammarSkills/cout_special_infor.cpp
+++ reuse/C++/GrammarSkills/cout_special_infor.cpp
@@ -1,8 +1,8 @@
 /* =============================================================================
-> File Name: cout_special_number.cpp
+> File Name: cout_special_infor.cpp
 > Author: DaiDai
 > Mail: daidai4269@aliyun.com
-> Created Time: Mon 23 Dec 2019 02:51:15 AM CST
+> Created Time: Thu 26 Dec 2019 02:18:58 PM CST
 ============================================================================= */
 
 
@@ -10,15 +10,28 @@
 
 using namespace std;
 
-
 int main() {
+  // number
   cout << 0x0 << endl;
   cout << 0x1 << endl;
-  
+
   cout << 0xbc9f1d34 << endl; // 16 binary
   cout << 0xffffffffU << endl;
 
+  // string
+  cout << "\\x00" << endl;
+  cout << "\\x0b" << endl;
+
+  cout << "\x00" << endl;
+  cout << "\x0b" << endl;
+
+  // sizeof
+  cout << "\\x00" << " --> " << "\x00" << " --sizeof--> " << sizeof("\x00") << endl;
+  cout << sizeof(void* ) << endl;
+  cout << sizeof(size_t) << endl;
+
   return 0;
 }
+
 
 
