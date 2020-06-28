c05d839c9c3d99b443f7b801d744bec22445b8d4
daidai21 <daidai4269@aliyun.com>
2020-06-28 23:10:55 +0800

    modify geography && add dict_as_cache

--- reuse/python3/algorithm/geography.py
+++ reuse/python3/algorithm/geography.py
@@ -11,6 +11,8 @@ def haversine_formula(origin:tuple, destination:tuple) -> float:
         distance of float type
     Ref:
         https://en.wikipedia.org/wiki/Haversine_formula
+    Disadvantages:
+        The greater the center angle, the greater the error.
     """
     from math import radians, sin, cos, sqrt, asin
 
@@ -35,3 +37,6 @@ def haversine_formula(origin:tuple, destination:tuple) -> float:
 if __name__ == "__main__":
     d = haversine_formula(tuple([45.7597, 4.8422]), tuple([48.8567, 2.3508]))
     assert(round(d, 3) == round(392.2172595594006, 3))
+
+    print(haversine_formula(tuple([0, 0]), tuple([180, 180])))
+    print(haversine_formula(tuple([0, 0]), tuple([90, 90])))
