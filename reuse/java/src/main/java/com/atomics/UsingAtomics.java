1251ca8597dda2b69daa8ccb520ce8b474875743
hu.fwh <hu.fwh@alibaba-inc.com>
2020-08-20 01:35:37 +0800

    modify java catalog && add java concurrent Thread, Atomic

---
+++ reuse/java/src/main/java/com/atomics/UsingAtomics.java
@@ -0,0 +1,48 @@
+// package com.atomics;
+
+
+/* ****************************************************************************
+ * File Name   : UsingAtomics.java
+ * Author      : DaiDai
+ * Mail        : daidai4269@aliyun.com
+ * Created Time: 四  8/20 01:17:39 2020
+ *************************************************************************** */
+
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class UsingAtomics {
+    public static void main(String[] args) throws InterruptedException {
+        AtomicCounter counter = new AtomicCounter();
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10000; ++i) {
+            cachedThreadPool.execute(() -> counter.increment());
+        }
+        cachedThreadPool.shutdown();
+        cachedThreadPool.awaitTermination(4000, TimeUnit.SECONDS);
+        System.out.println("Result shound be 10000: Actual result is: " + counter.get());
+    }
+
+    /**
+     * A counter using AtomicInteger
+     */
+    static class AtomicCounter {
+        private AtomicInteger atomicInteger = new AtomicInteger();
+
+        public void increment() {
+            atomicInteger.incrementAndGet();
+        }
+
+        public void decrement() {
+            atomicInteger.decrementAndGet();
+        }
+
+        public int get() {
+            return atomicInteger.get();
+        }
+    }
+}
---
+++ reuse/java/src/main/java/com/threads/UsingThreads.java
@@ -0,0 +1,35 @@
+// package com.threads;
+
+
+/* ****************************************************************************
+ * File Name   : UsingThreads.java
+ * Author      : DaiDai
+ * Mail        : daidai4269@aliyun.com
+ * Created Time: 四  8/20 00:55:51 2020
+ *************************************************************************** */
+
+
+public class UsingThreads {
+    public static void main(String[] args) throws InterruptedException {
+        // Creating
+        Thread created = new Thread();
+        created.start();
+
+        // Assigning a task for running on a thread
+        Thread threadWithTask = new Thread(
+            () -> System.out.println("Inside thread: " + Thread.currentThread().getName())
+        );
+        threadWithTask.start();
+
+        // Interrupting a thread
+        Runnable interrupatblyTask = () -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("I'm not interrupted " + Thread.currentThread().getName());
+            }
+        };
+        Thread interruptable = new Thread(interrupatblyTask);
+        interruptable.start();
+        Thread.sleep(3000);
+        interruptable.interrupt();
+    }
+}
