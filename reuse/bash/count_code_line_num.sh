6d14542f20f1e552acf4edb009b9acf5e8a00848
hu.fwh <hu.fwh@alibaba-inc.com>
2020-09-29 21:30:06 +0800

    debug count_code_line_num.sh  (xxx_line_num == 0 err)

--- reuse/bash/count_code_line_num.sh
+++ reuse/bash/count_code_line_num.sh
@@ -19,11 +19,19 @@ else
 fi
 
 
-cpp_line_num=$(find $path -type f | grep -E "\.cpp|\.hpp" | xargs wc -l | awk '{print $1}' | awk 'END {print}' )
+cpp_line_num=$(find $path -type f | grep -E "\.cpp|\.hpp|\.hh|\.cc|\.hxx" | xargs wc -l | awk '{print $1}' | awk 'END {print}' )
 c_line_num=$(find $path -type f | grep -E "\.c|\.h" | xargs wc -l | awk '{print $1}' | awk 'END {print}' )
 py_line_num=$(find $path -type f | grep "\.py" | xargs wc -l | awk '{print $1}' | awk 'END {print}' )
 sh_line_num=$(find $path -type f | grep "\.sh" | xargs wc -l | awk '{print $1}' | awk 'END {print}' )
 java_line_num=$(find $path -type f | grep "\.java" | xargs wc -l | awk '{print $1}' | awk 'END {print}' )
+
+
+cpp_line_num=${cpp_line_num:-0}
+c_line_num=${c_line_num:-0}
+py_line_num=${py_line_num:-0}
+sh_line_num=${sh_line_num:-0}
+java_line_num=${java_line_num:-0}
+
 
 echo "***** Code Total Result: *****"
 echo "C++: $cpp_line_num"
