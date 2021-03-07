f44bc1be94547e4d9cfd0743e5823202f27564fa
hu.fwh <hu.fwh@alibaba-inc.com>
2021-03-07 18:52:14 +0800

    .

---
+++ codelib/cpp11/test/def_test.cpp
@@ -0,0 +1,19 @@
+/* ****************************************************************************
+ * File Name   : def_test.cpp
+ * Author      : DaiDai
+ * Mail        : daidai4269@aliyun.com
+ * Created Time: 日  3/ 7 18:51:30 2021
+ *************************************************************************** */
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+  return 0;
+}
+
