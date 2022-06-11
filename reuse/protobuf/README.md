aadd95600ff3476b76390fc05d6f55e31e55cf82
fuweihu.fwh <fuweihu.fwh@bytedance.com>
2022-06-11 16:30:19 +0800

    .

---
+++ reuse/protobuf/README.md
@@ -0,0 +1,6 @@
+
+V2和V3语法有差异，应该不兼容
+
+https://colobu.com/2019/10/03/protobuf-ultimate-tutorial-in-go/
+https://www.tizi365.com/archives/367.html
+https://www.zhangjiee.com/topic/grpc/write-proto-spec.html
--- reuse/sql/reuse.sql
+++ reuse/sql/reuse.sql
@@ -14,7 +14,7 @@ SHOW tables;
 CREATE TABLE IF NOT EXISTS `test` (
     `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
     `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+    `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON CURRENT_TIMESTAMP COMMENT '更新时间',
     `deleted_time` datetime DEFAULT NULL COMMENT '删除时间',
     `note` varchar(500) DEFAULT NULL COMMENT '备注',
 
