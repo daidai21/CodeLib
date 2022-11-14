e2823eb6a884d9d002315a5804535a6f3abe74b1
fuweihu.fwh <fuweihu.fwh@bytedance.com>
2022-11-14 13:26:46 +0800

    .

--- reuse/hsql/reuse.sql
+++ reuse/hsql/reuse.sql
@@ -1,5 +1,17 @@
 
 -- 多行合并
 select "tom" as name
-union select "jerry" as name
+union 
+select "jerry" as name
+;
+
+-- 建表
+
+CREATE TABLE IF NOT EXISTS example_df(
+    id INT COMMENT 'ID'
+    ,name STRING COMMENT '姓名'
+    ,age INT COMMENT '年龄'
+    ,phone STRING COMMENT '电话'
+) PARTITIONED BY date STRING COMMENT '日期'
+LIFECYCLE 31 -- 生命周期
 ;
